d082ff3b84dec075ffdc7fd71a5f310acd07966b
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-05 02:10:19 +0530

    Refactor environment variable checks and clean up config.js

--- src/config/config.js
+++ src/config/config.js
@@ -5,21 +5,38 @@ if (!process.env.MONGO_URI) {
   throw new Error("MONGO_URI is not define in environment variables");
 }
 
-// if(!process.env.REFRESH_TOKEN){
-//     throw new Error("REFRESH_TOKEN is not define in environment variables")
-// }
-// if(!process.env.ACCESS_TOKEN){
-//     throw new Error("ACCESS_TOKEN is not define in environment variables")
-// }
-
 if (!process.env.JWT_SCRET) {
   throw new Error("JWT_SCRET is not define in environment variables");
 }
 
+if (!process.env.GOOGLE_CLIENT_SECRET) {
+  throw new Error(
+    "GOOGLE_CLIENT_SECRET is not define in environment variables",
+  );
+}
+
+if (!process.env.GOOGLE_CLIENT_SECRET) {
+  throw new Error(
+    "GOOGLE_CLIENT_SECRET is not define in environment variables",
+  );
+}
+
+if (!process.env.GOOGLE_REFRESH_TOKEN) {
+  throw new Error(
+    "GOOGLE_REFRESH_TOKEN is not define in environment variables",
+  );
+}
+
+if (!process.env.GOOGLE_USER) {
+  throw new Error("GOOGLE_USER is not define in environment variables");
+}
+
 const config = {
   MONGO_URI: process.env.MONGO_URI,
-  // REFRESH_TOKEN:,
-  // ACCESS_TOKEN:,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  GOOGLE_REFRESH_TOKEN: process.env.GOOGLE_REFRESH_TOKEN,
+  GOOGLE_USER: process.env.GOOGLE_USER,
   JWT_SCRET: process.env.JWT_SCRET,
 };
 
